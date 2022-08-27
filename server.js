de917ff6d0a9229e3acfd6c338a7f2ba94569ead
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-27 15:36:18 +0200

    now good

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const emailExistence = require("email-existence");
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get("/check", (req, res) => {
+app.get("/", (req, res) => {
   const { email } = req.query;
   if (!email) {
     return res.status(400).send("enter your email");
