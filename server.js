dec64b7125915b5985e98c427d73ce87fb4baf7a
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-28 00:19:58 +0200

    added pdf to html converter

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const emailExistence = require("email-existence");
 const app = express();
 const PORT = process.env.PORT || 5000;
+var pdfcrowd = require("pdfcrowd");
+
+var client = new pdfcrowd.PdfToHtmlClient(
+  "giovanni",
+  "049eff6854c12a54a2996a9ea8856d27"
+);
+const fs = require("fs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   const { email } = req.query;
   if (!email) {
@@ -20,6 +29,23 @@ app.get("/", (req, res) => {
     }
     return res.status(200).send(false);
   });
+});
+app.post("/pdf", (req, res) => {
+  let { pdf } = req.body;
+  let createRandomname = () => {
+    return (
+      Math.random().toString(36).substring(7) + pdf.slice(pdf.lastIndexOf("/")-5)
+    ).replace("/", "").replace(".pdf", ".html");
+  };
+  let files = fs.readdirSync("./");
+  if (files.includes(createRandomname())) {
+    createRandomname();
+  } else {
+    client.convertUrlToFile(pdf, createRandomname(), function (err, fileName) {
+      if (err) return res.status(400).send("something wen wrong", err);
+      res.status(200).send(req.hostname + "/" + fileName);
+    });
+  }
 });
 const server = http.createServer(app);
 server.listen(PORT, () => {
