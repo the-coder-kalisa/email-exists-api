8e7cf18f029514077cd665e77ac70760e63c9ed8
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-27 15:28:47 +0200

    now good

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const emailExistence = require("email-existence");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/check", (req, res) =>{
+    const {email} = req.query;
+    if(!email){
+     return    res.status(400).send("enter your email");
+    }
+    if(!/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm.test(email)){
+return res.status(400).send("enter a valid email");
+    }
+    emailExistence.check(email, (error, response) =>{
+        if(error){
+          return  res.status(400).send(error);
+        }
+        if(response){
+         return   res.status(200).send("Email is available");
+        }
+        return res.status(400).send("Email not found");
+    })
+})
+const server = http.createServer(app);
+server.listen(PORT, () =>{console.log("server started on port " + PORT)})
